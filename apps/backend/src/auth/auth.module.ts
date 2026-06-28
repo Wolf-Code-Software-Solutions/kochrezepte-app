2af1712fe00cc0ee9925aaa753aac26faf04a6e2
Hermes Orchestrator <hermes@koch.de>
2026-06-28 16:23:49 +0200

    [AUTH-001]: HashService mit bcrypt (10 salt rounds) - hash() und compare() + Unit-Tests (6 Tests, alle passing)

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HashService } from './hash.service';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
@@ -15,7 +16,7 @@ import { JwtStrategy } from './jwt.strategy';
       }),
     }),
   ],
-  providers: [JwtStrategy],
-  exports: [JwtModule, JwtStrategy],
+  providers: [HashService, JwtStrategy],
+  exports: [HashService, JwtModule, JwtStrategy],
 })
 export class AuthModule {}
